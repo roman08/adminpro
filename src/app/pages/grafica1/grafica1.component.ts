5e7e9fda3aa8e54c8788b0dca4bcca35c46a7311
Roman Madrigal <rmcentinela@gmail.com>
2022-12-02 17:57:31 -0600

    se agrega graficas y barra de progreso

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
+  public doughnutChartLabels = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public data = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#9E120E', '#FF5800', '#FFB414'],
+      },
+    ],
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  // TODO: GRAFICA2
+  public doughnutChartLabels2 = ['Ciudad', 'Estado', 'Pais'];
+  public data2 = {
+    labels: this.doughnutChartLabels2,
+    datasets: [
+      {
+        data: [25, 230, 150],
+        backgroundColor: ['#9E120E', '#FF5800', '#FFG414'],
+      },
+    ],
+  };
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
 
 
 
@@ -22,6 +26,13 @@ import { RouterModule } from '@angular/router';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule,
+    ComponentsModule,
+    
+  ],
 })
 export class PagesModule {}
